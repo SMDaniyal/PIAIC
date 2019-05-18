590803ff9e5e39f272d53b5428bd5d13578fd4cc
Syed Muhammad Daniyal (SMD) <m_daniyal28@hotmail.com>
2019-05-18 14:38:40 +0500

    Class 2

---
+++ Python/Class 2/String.py
@@ -0,0 +1,62 @@
+# a = "Hello World"
+
+# String Functions
+
+# a.capitalize()  # capitalize the string
+# a.upper()   # upper case the string
+# a.title()     # Upper case the First letter of words
+# a.lower()       #lower case the string
+
+# Multi-Line Variable
+# Hard coded
+
+# abc = """Syed Muhammad Daniyal
+# Batch # 1
+# PIAIC"""
+# print(abc)
+
+# Taking Input
+
+# name = input("Enter Your Name: ")
+# f_name = input("Enter Your Father Name: ")
+# Batch = input("Enter Batch Number: ")
+
+# temp = """
+# *************************
+#    Student Information
+# *************************
+# Name : {}
+# f_name : {}
+# Batch = {}
+# *************************"""
+
+# print(temp.format(name,f_name,Batch))
+
+# Operator
+# Solve via PEMDAS
+
+
+#   Normal Operators
+#   + , - , * , / , % , // , **
+
+# a = 10 / 2      # normal division   Ans will be 5.0
+# b = 10 // 2     # FLoat Division (Remove Float answer) Ans will be 5
+# c = 2 ** 3      # Power Funtions
+
+#   Assignment Operator
+#   =, += , -= , *=, %= ,/=
+
+# a = "Hello World"
+# a += " Chai pee lo"
+
+# a = 10
+# a %= 3 
+# print(a)
+
+#   Logocal Operator
+
+
+
+
+
+
